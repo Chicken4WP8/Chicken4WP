01cf8c5da8102fc5d93ddcc372068b7e781c92b8
im_wower <george674834080@gmail.com>
2014-05-14 00:28:32 +0800

    image cache

---
+++ Chicken4WP/Controls/StringToImageSourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Chicken4WP.Services;
+
+namespace Chicken4WP.Controls
+{
+    public class StringToImageSourceConverter: IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ImageCacheService.GetImageFromUrl(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chicken4WP/Services/ImageCacheService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chicken4WP.Services
+{
+    public class ImageCacheService
+    {
+        private static Dictionary<string, BitmapImage> cachedImages = new Dictionary<string, BitmapImage>();
+
+        public static BitmapImage GetImageFromUrl(string url)
+        {
+            if (cachedImages.ContainsKey(url))
+            {
+                return cachedImages[url];
+            }
+            var request = WebRequest.CreateHttp(url + "?random=" + DateTime.Now.Ticks.ToString("x"));
+            var task = Task.Factory.FromAsync(
+                request.BeginGetResponse,
+                asyncResult => request.EndGetResponse(asyncResult),
+                null);
+            return task.ContinueWith(t => DownloadImage(t.Result, url)).Result;
+        }
+
+        private static BitmapImage DownloadImage(WebResponse response, string url)
+        {
+            using (Stream stream = response.GetResponseStream())
+            {
+                var image = new BitmapImage();
+                image.SetSource(stream);
+                cachedImages.Add(url, image);
+            }
+            response.Close();
+            return cachedImages[url];
+        }
+    }
+}
