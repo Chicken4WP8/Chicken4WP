32af7136539d56df913e01f40c0528e7bade937a
im_wower <george674834080@gmail.com>
2014-05-24 18:30:16 +0800

    tweet and retweet

---
+++ Chicken4WP/Controls/TemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Chicken4WP.Models;
+
+namespace Chicken4WP.Controls
+{
+    public abstract class TemplateSelector : ContentControl
+    {
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+            ContentTemplate = this.SelectTemplate(this, newContent);
+        }
+        protected virtual DataTemplate SelectTemplate(DependencyObject sender, object newValue)
+        {
+            return null;
+        }
+    }
+
+    public class TweetTemplateSelector : TemplateSelector
+    {
+        public DataTemplate TweetTemplate { get; set; }
+
+        public DataTemplate RetweetTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplate(DependencyObject sender, object newValue)
+        {
+            Tweet tweet = newValue as Tweet;
+            if (tweet.RetweetStatus == null)
+                return TweetTemplate;
+            return RetweetTemplate;
+        }
+    }
+}
